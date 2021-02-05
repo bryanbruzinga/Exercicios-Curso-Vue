dcd39fc164f65962c94e5468300d08c6aa69eb7a
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-02-05 14:48:27 -0300

    primeiro app .vue

---
+++ app-techno/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <modal-principal>
+      <template v-slot:header="{ logo }">
+       Esse é o header
+       {{logo}}
+      </template>
+      <template v-slot:default="{ user }">
+        {{user.nome}}
+        <p>Esse é um teste.</p>
+        <button>Comprar</button>
+      </template>
+      <template #footer>
+        <p>Esse é o footer</p>
+      </template>
+    </modal-principal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from './components/ModalPrincipal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ModalPrincipal
+  }
+}
+</script>
---
+++ app-techno/src/components/ModalPrincipal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="modal">
+    <slot name="header" :logo="logo"></slot>
+   <h1>Esse é o modal</h1>
+   <slot :user="user"></slot>
+   <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalPrincipal',
+  data() {
+    return {
+      logo: 'Techno',
+      user: {
+        nome: 'Bryan'
+      }
+    }
+  }
+}
+</script>
