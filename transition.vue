f231b312df17b018bee6bab6a21438662e3921e6
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-02-07 19:29:32 -0300

    primeiras animações Vue

---
+++ transition.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <button @click="ativo= !ativo">Ativar</button>
+    <transition name="fade">
+        <h1 v-if="ativo">Serviços</h1>
+    </transition>
+    <transition appear name="fade">
+         <h2>Atividades</h2>
+    </transition>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            ativo: false
+        }
+    }
+}
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .8s;
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+
+</style>
