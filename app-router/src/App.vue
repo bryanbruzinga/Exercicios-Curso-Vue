8ed087624db0493c9d03ebc553469505d3422eac
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-02-08 09:56:36 -0300

    aula de Vue Routers

---
+++ app-router/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Logo</h1>
+    <nav>
+      <router-link to='/'>Home</router-link>
+      <router-link to='/cursos'>Cursos</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+a {
+  padding: 10px;
+}
+</style>
---
+++ app-router/src/views/Cursos.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Essa é a página Cursos</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
